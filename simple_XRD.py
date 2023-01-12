5e7ccfdbbf6b2bfc1c9ce7eaa4281243277f04d4
Hezy Amiel <hezy.amiel@gmail.com>
2023-01-12 22:57:36 +0200

    started Status.md

--- simple_XRD.py
+++ simple_XRD.py
@@ -4,23 +4,6 @@
 """
 Created on Wed Dec  7 14:56:13 2022
 @author: Yehezkel Amiel
-
-How is it now? (2023-01-10)
-    seems to work fine for cubic symmetries (sc, bcc, fcc) with one element
-
-Things to do (short term):
-    [] function "intensity" should not use built in values
-    [] function "make_graph" should give nicer titles
-    [] add random noise
-    [] add bacground
-    [] investigate U,V,W values
-    [] export to csv
-    [] read setup and sample data from external files
-
-For the future (long term):
-    [] find Miller indecies based on lattice vectors rather than selection rules
-    [] other symmetries
-    [] more than one element    
 """
 
 ''' Importing lipreries '''

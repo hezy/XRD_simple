41e160aad8ed0ab040ff6f0f4ae38787d27c60e2
Hezy <hezy.amiel@gmail.com>
2023-01-15 17:09:30 +0200

    fixed Voigt

--- pseudo-voigt-VS.jl
+++ pseudo-voigt-VS.jl
@@ -4,21 +4,21 @@
 using Plots
 using SpecialFunctions
 
-x = range(start=-10, stop=10, step=0.01)
+x = range(start=-3, stop=3, step=0.01)
 
 function Gaussian(x, fwhm)
-	g1 = 2.0 * sqrt(log(2)/π)
-	g2 = 4.0 * log(2)
-    σ = fwhm/(2sqrt(2log(2)))
+	#g1 = 2.0 * √(log(2)/π)
+	#g2 = 4.0 * log(2)
     #return @. (g1 / fwhm) * exp((-g2 * x^2) / (fwhm^2)) 
-    return @. 1/sqrt(2π)/σ * exp(-x^2/2σ)
+    σ = fwhm/(2√(2log(2)))
+    return @. 1/√(2π)/σ * exp(-x^2/2σ^2)
 end
 
 function Lorentzian(x, fwhm)
-	l1 = 2/π
-	l2 = 4.0
+	#l1 = 2/π
+	#l2 = 4.0
+    #return @. (l1 / fwhm) / ((1 + l2 * x^2) / fwhm^2 ) 
     γ = fwhm / 2
-    # return @. (l1 / fwhm) / ((1 + l2 * x^2) / fwhm^2 ) 
     return @. (γ/pi) / (x^2 + γ^2)
 end
 
@@ -28,22 +28,23 @@ end
 
 function Voigt(x, fwhm_L, fwhm_G)
     γ = fwhm_L/2
-    σ = fwhm_G/2
-    z = @. -1im * (x + 1im * γ) / (sqrt(2) * σ)
-    return @. real(erfcx(z)) / (sqrt(2pi) * σ)
+    σ = fwhm_G/(2√(2log(2)))
+    z = @. -im * (x + im * γ) / (√2 * σ)
+    return @. real(erfcx(z)) / (√(2pi) * σ)
 end
-#erfcx
+
 
 y1 = Lorentzian(x, 1)
 y2 = Gaussian(x, 1)
 y3 = pseudo_Voigt(x, 1, 0.5)
-y4 = Voigt(x, 0.01, 1)
+y4 = Voigt(x, 0.45, 0.72)
 
 p = plot(x,[y1 y2 y3 y4], label=["Lorentzian" "Gaussian" "Pseudo Voigt" "Voigt"])
+#p = plot(x,[y1 y2 y3], label=["Lorentzian" "Gaussian" "Pseudo Voigt"])
 title!("peak functions")
-xlabel!("xii")
-ylabel!("y")
+xlabel!(raw"x")
+ylabel!(raw"y")
 
-display(p)
+#display(p)
 
 #savefig(plot,"file.png")

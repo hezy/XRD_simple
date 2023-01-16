b698fa5b3d25f77222a5f8ece6d500d278ccd5cd
Hezy Amiel <hezy.amiel@gmail.com>
2023-01-16 21:11:53 +0200

    /

--- Lorenzian and Gaussian.jl
+++ Lorenzian and Gaussian.jl
@@ -1,42 +1,42 @@
 using SymPy
 
 x = Sym("x") 
-gamma, sigma = Sym("gamma sigma")
+γ , σ = Sym("γ σ")
 
-function lorentz(x, gamma)
+function lorentz(x, γ)
     # non-normnormalized Lorentzian
-    return 1/(gamma^2 + x^2)
+    return 1/(γ^2 + x^2)
 end
 
-# Finding the norm of Lorentzian
-L_int = sympy.Integral(lorentz(x,gamma), (x, -oo, oo))
+print("Finding the norm of Lorentzian:")
+L_int = sympy.Integral(lorentz(x,γ), (x, -oo, oo))
 L_norm = L_int.doit()
-#display(Eq(L_int, L_norm ))
+print(Eq(L_int, L_norm ))
 
-# calculating the half width at half maximum (HWHM) of a Lorenzian
-L_max = lorentz(0,gamma)
-L_HWHM = Eq(lorentz(x,gamma),(1//2*L_max))
-#display(L_HWHM)
+print("calculating the half width at half maximum (HWHM) of a Lorenzian:")
+L_max = lorentz(0,γ)
+L_HWHM = Eq(lorentz(x,γ),(1//2*L_max))
+print(L_HWHM)
 solve(L_HWHM, x)
-#display(solve(L_HWHM, x))
+print(solve(L_HWHM, x))
 
 
-function gauss(x,sigma)
+function gauss(x, σ)
 # non-normalized Gaussian
-    return exp(-x^2/2sigma^2)
+    return exp(-x^2/2σ^2)
 end
 
-# finding the norm of Gaussian
-G_int = sympy.Integral(gauss(x,sigma), (x, -oo, oo))
+print("Finding the norm of Gaussian:")
+G_int = sympy.Integral(gauss(x,σ), (x, -oo, oo))
 G_norm = G_int.doit()
-#display(Eq(G_int, G_norm))
+print(Eq(G_int, G_norm))
 
 # calculating the half width at half maximum (HWHM) of a Gaussian
-G_max = gauss(0, sigma)
-G_HWHM = Eq(gauss(x,sigma),1//2*G_max)
-#display(G_HWHM)
+G_max = gauss(0, σ)
+G_HWHM = Eq(gauss(x,σ),1//2*G_max)
+print(G_HWHM)
 solve(G_HWHM, x)
-#display(solve(G_HWHM, x))
+print(solve(G_HWHM, x))
 
 
 # example: generating LaTeX code for the displayed equations:

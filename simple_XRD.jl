a1a73b2267128e385f6ce58e9e0cc5fcbec8ab87
Hezy Amiel <hezy.amiel@gmail.com>
2023-04-06 22:47:40 +0300

    deleted:    BCC.png 	deleted:    FCC.png 	deleted:    SC.png 	modified:   simple_XRD.jl

--- simple_XRD.jl
+++ simple_XRD.jl
@@ -51,10 +51,14 @@ Functions
 # end
 
 
-# function peak(θ, θ₀, A, w, n)
-#     return @. A * pseudo_Voigt(θ-θ₀, w, n)
-#     # return @. A * Voigt(θ-θ₀, w, n)
-# end
+function Voigt_peak(θ::Vector, θ₀, A, w_L, w_G, n)
+    """Returns a Voigt peak centered around θ₀, with amplitude A, width w, and mixing factor n """
+    """untested"""
+    γ = w_L / 2
+    σ = w_G / (2√(2log(2)))
+    z = @. -im * ((θ - θ₀) + im * γ) / (√2 * σ)
+    return @. real(erfcx(z)) / (√(2pi) * σ)
+end
 
 
 function pseudo_Voigt_peak(θ::Vector, θ₀, A, w, n)

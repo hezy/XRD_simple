52d9d93b0d5d4348d2723eb91677f792b2b2f71d
Hezy Amiel <hezy.amiel@gmail.com>
2024-11-26 01:55:59 +0200

    testing examples

---
+++ example_use_Voigt.jl
@@ -0,0 +1,13 @@
+using Plots
+
+include("functions.jl")
+
+θ = collect(range(2.0, 160.0, 1000))
+
+y1 = Voigt_peak(θ, 25.0, 10.0, 4.0, 4.0)
+y2 = pseudo_Voigt_peak(θ, 75.0, 10.0, 4.0, 4.0)
+# y = y1 + y2
+
+plot(θ, y1)
+plot!(θ, y2)
+# plot!(θ, y)
